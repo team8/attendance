73ab3ad315afbae34b97b4efff14d4d8c1328006
Peter <pcturnbull7@gmail.com>
2015-10-15 09:49:06 -0700

    Adding Django models
    Doing some initial work, creating some starter objects that the system
    will track. Baby steps.

--- attendance-website/attendanceapp/models.py
+++ attendance-website/attendanceapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Subteam(models.Model):
+    name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+
+class Student(models.Model):
+    name = models.CharField()
+    studentID = models.IntegerField()
+    subteam = models.ForeignKey(Subteam)
