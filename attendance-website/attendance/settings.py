03902662a2f3798df7f15d458c40463d40ebe6d3
Peter <pcturnbull7@gmail.com>
2016-01-12 09:58:24 -0800

    Disabled modwsgi in settings

--- attendance-website/attendance/settings.py
+++ attendance-website/attendance/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'attendanceapp',
-    'mod_wsgi.server',
+    # 'mod_wsgi.server', Not currently in use.
 )
 
 MIDDLEWARE_CLASSES = (
