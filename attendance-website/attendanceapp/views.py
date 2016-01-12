ca43fdce2cef6a76e3f710d3a360e9c3abe9a316
Peter <pcturnbull7@gmail.com>
2016-01-12 13:06:55 -0800

    Returned makeNewStudent to original state
    I was playing around with it a bit, didn't really figure it out. Much
    sad :(

--- attendance-website/attendanceapp/views.py
+++ attendance-website/attendanceapp/views.py
@@ -70,11 +70,8 @@ def logOut(student):
 def makeNewStudent(ID):
 
     html = requests.post("https://palo-alto.edu/Forgot/Reset.cfm",data={"username":str(ID)}).text
-    name = re.search(r'<input name="name" type="hidden" label="name" value=(.*?)"',html)
-    if name is None:
-        return None
-    culledName = name.group(1)
-    Student(name=culledName,studentID=ID,subteam=Subteam.objects.get(name="Unknown")).save()
+    name = re.search(r'<input name="name" type="hidden" label="name" value=(.*?)"',html).group(1)
+    Student(name=name,studentID=ID,subteam=Subteam.objects.get(name="Unknown")).save()
     return True
     #return False
 
