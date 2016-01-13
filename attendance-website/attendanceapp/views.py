344a392679fc3cae71dcfa478f45c100bba38ec4
Peter <pcturnbull7@gmail.com>
2016-01-12 18:45:39 -0800

    Fixed!
    (At least, I'm pretty sure this time :) )

--- attendance-website/attendanceapp/views.py
+++ attendance-website/attendanceapp/views.py
@@ -91,8 +91,8 @@ def logInPage(request):
 
     except:
         if makeNewStudent(request.POST['studentID']) == False:
--            print "make student failing"
-             return render(request, 'attendanceapp/ScanCard.html', {'message':"Sorry, student ID# not found."})
+            print "make student failing"
+            return render(request, 'attendanceapp/ScanCard.html', {'message':"Sorry, student ID# not found."})
 
     #if len(studentID)==4:
     #    if studentID=="8888":
