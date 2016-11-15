252e50581463dae15855bec3b38030ebf310fe23
bdavies01 <bertdaviezz@gmail.com>
2016-11-15 01:28:35 +0000

    Changes to merge with master.
    Turned off debug mode for master.

--- attendance-website/attendanceapp/tables.py
+++ attendance-website/attendanceapp/tables.py
@@ -5,6 +5,6 @@ class StudentTable(tables.Table):
 	class Meta:
 		model = Student
 		#attrs = {"class": "name"}
-		row_attrs = {"data-id": lambda record: record.pk}
+		#row_attrs = {"data-id": lambda record: record.pk}
 		exclude=("lastLoggedIn", "atLab", "studentID", "id")
 		per_page = "100"
