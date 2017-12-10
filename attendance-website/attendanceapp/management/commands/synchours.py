bd7e109945542af318ff4379522ccfe126d5fafe
Devin Ardeshna <devin@ardeshna.com>
2017-12-09 23:47:39 -0800

    updated synchours to pull from google sheets

--- attendance-website/attendanceapp/management/commands/synchours.py
+++ attendance-website/attendanceapp/management/commands/synchours.py
@@ -1,42 +1,41 @@
-from __future__ import print_function
 from django.core.management.base import BaseCommand, CommandError
-from apiclient.discovery import build
-from httplib2 import Http
 from pytz import timezone
-from oauth2client import file, client, tools
 from attendanceapp.models import LabHours, OverallStats
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date, time
 from attendanceapp.util import utc_to_normal
-import urllib
+import urllib2
+import csv
 import re
 
 class Command(BaseCommand):
-    help = 'updates the lab hours weekly'
+    help = 'updates the lab hours from google sheet'
     def handle(self, *args, **options):
-        SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-        store = file.Storage('/home/bdavies/.credentials/calendar-python-quickstart.json')
-        creds = store.get()
-        if not creds or creds.invalid:
-            flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
-            creds = tools.run_flow(flow, store, flags) \
-                    if flags else tools.run(flow, store)
-        CAL = build('calendar', 'v3', http=creds.authorize(Http()))
-        datetime_phrase = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S') #i honestly hate time formats jeez
-        datetime_phrase += 'Z'
-        week_from_now = (datetime.utcnow() + timedelta(days = 7)).strftime('%Y-%m-%dT%H:%M:%S')
-        week_from_now += 'Z'
-        events = CAL.events().list(calendarId="db25n7oev1at726gljle5d784c@group.calendar.google.com", singleEvents = True, timeMin = datetime_phrase, timeMax = week_from_now).execute()
-        for event in events['items']:
-            if event['summary'].startswith("LAB OPEN"):
-                LabHours.objects.create(name=event['summary'], starttime = event['start'].get("dateTime"), endtime=(datetime.strptime(event['end'].get('dateTime').replace(' ', '')[:-6], '%Y-%m-%dT%H:%M:%S')+timedelta(minutes=67)).replace(tzinfo=timezone("America/Los_Angeles")), totalTime = getTimeDiff(event))
-def getTimeDiff(event):
-    a = event['start'].get('dateTime').replace(' ', '')[:-6]
-    b = event['end'].get('dateTime').replace(' ', '')[:-6]
-    aobj = datetime.strptime(a, '%Y-%m-%dT%H:%M:%S')
-    bobj = datetime.strptime(b, '%Y-%m-%dT%H:%M:%S')
-    c = bobj-aobj
-    minutes, seconds = divmod(c.total_seconds(), 60)
-    hours, minutes = divmod(minutes, 60)
-    minutesdecimal = minutes/60
-    result = hours+minutesdecimal+1
-    return result
+    
+    	response = urllib2.urlopen("https://docs.google.com/spreadsheets/d/e/2PACX-1vQS83DMkhEnoiz8EmjVyP40DV-HbUkZkKiGZbjKZKvlIC6cdBlYOJJcb6NfS2LuXHnZz_OT0EsxhevW/pub?gid=1449894080&single=true&output=csv")
+    	data = csv.DictReader(response)
+    	data = [x for x in data if datetime.strptime(x['Date'], "%m/%d/%Y") >= datetime.combine(date.today(), datetime.min.time())]
+    	
+    	
+    	for i in data:
+    		print i
+    		
+    	for x in data:
+    	
+    		starttime = datetime.combine(datetime.strptime(x['Date'], "%m/%d/%Y").date(), datetime.strptime(x['Start Time'], "%I:%M %p").time())
+    		endtime = datetime.combine(datetime.strptime(x['Date'], "%m/%d/%Y").date(), datetime.strptime(x['End Time'], "%I:%M %p").time())
+    		
+    		hours = LabHours.objects.all().filter(starttime__gt=datetime.strptime(x['Date'], "%m/%d/%Y"), starttime__lt=datetime.strptime(x['Date'], "%m/%d/%Y")+timedelta(days=1))
+    		
+    		if not hours:
+    			LabHours.objects.create(name="", starttime=starttime, endtime=endtime)
+    		elif len(hours) == 1:
+    			if (hours[0].starttime != starttime or hours[0].endtime != endtime):
+    				hours[0].starttime = starttime
+    				hours[0].endtime = endtime
+    				hours[0].save()
+    				
+    		else:
+    			hours.delete()
+    			LabHours.objects.create(name="", starttime=starttime, endtime=endtime)
+    			
+    		#does not update total time for lab hours
