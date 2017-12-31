4def7cdebf5afb91ee430452c7b55d7868f59a86
Devin Ardeshna <devin@ardeshna.com>
2017-12-31 10:36:38 -0800

    updated logout command

--- attendance-website/attendanceapp/management/commands/addstudents.py
+++ attendance-website/attendanceapp/management/commands/addstudents.py
@@ -9,22 +9,22 @@ class Command(BaseCommand):
     help = 'adds students from csv file'
     def handle(self, *args, **options):
     
-    	file = open(os.path.join(settings.BASE_DIR, "data2.csv"))
-    	data = list(csv.DictReader(file))
-    	
-    	subteams = []
-    	for x in data:
-    		if x['Subteam'] not in subteams:
-    			subteams.append(x['Subteam'])	
-    	for x in subteams:
-    		existing_subteams = Subteam.objects.all().filter(name=x).first()
-    		if not existing_subteams:
-    			Subteam.objects.create(name=x)
-    	
-    	for x in data:
-    		students = Student.objects.all().filter(studentID=x['ID#']).first()
-    		if not students:
-    			Student.objects.create(name=x['Full Name'], studentID=x['ID#'], subteam=Subteam.objects.get(name=x['Subteam']), slackID=x['Slack ID'])
-    		else:
-    		students.slackID = x['Slack ID']
-    		students.save()
+        file = open(os.path.join(settings.BASE_DIR, "data2.csv"))
+        data = list(csv.DictReader(file))
+        
+        subteams = []
+        for x in data:
+            if x['Subteam'] not in subteams:
+                subteams.append(x['Subteam'])   
+        for x in subteams:
+            existing_subteams = Subteam.objects.all().filter(name=x).first()
+            if not existing_subteams:
+                Subteam.objects.create(name=x)
+        
+        for x in data:
+            students = Student.objects.all().filter(studentID=x['ID#']).first()
+            if not students:
+                Student.objects.create(name=x['Full Name'], studentID=x['ID#'], subteam=Subteam.objects.get(name=x['Subteam']), slackID=x['Slack ID'])
+            else:
+                students.slackID = x['Slack ID']
+                students.save()
--- attendance-website/attendanceapp/management/commands/logout.py
+++ attendance-website/attendanceapp/management/commands/logout.py
@@ -15,29 +15,29 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for person in Student.objects.all():
             if person.atLab:
-                #logOut(person)
-                dm_id = CLIENT.api_call("im.open", user="U039ZJW8K", return_im=True)['channel']['id']
+                logOut(person)
+                dm_id = CLIENT.api_call("im.open", user=person.slackID, return_im=True)['channel']['id']
                 message = "Hi " + person.name.split(" ")[0] + "--you forgot to log out before leaving the lab yesterday (" + (date.today()-timedelta(days=1)).strftime("%m/%d/%y") + ").  Please contact <@U039ZJW8K> to have your attendance corrected in the system."
                 CLIENT.api_call("chat.postMessage", channel=dm_id, text=message, as_user=True)
                 
-    def logOut(student):
-        #Tell the system that the student is no longer in the lab
-        student.atLab=False
+def logOut(student):
+    #Tell the system that the student is no longer in the lab
+    student.atLab=False
 
-        #load the last logged in time into memory
-        lastLoggedIn=student.lastLoggedIn
-        timeNow=lastLoggedIn
-    
-        #Move to hoursWorked model
-        hours_elapsed = 0
-        valid_hours_elapsed = 0
-        percentTime = 0
-        weight = 0
-    
-        timeWorked=HoursWorked(timeIn=lastLoggedIn,day = timeNow.strftime("%A"),timeOut=timeNow, totalTime=hours_elapsed, validTime=valid_hours_elapsed, autoLogout=True, percentTime = percentTime, weight = weight, owner = student)
-        timeWorked.save()
-        #add the time worked object to the student so it can be viewed in the calander
-        student.hoursWorked.add(timeWorked)
-        #add the minutes to the student's total time
-        student.save()
-        do_student_calcs(student)
+    #load the last logged in time into memory
+    lastLoggedIn=student.lastLoggedIn
+    timeNow=lastLoggedIn
+
+    #Move to hoursWorked model
+    hours_elapsed = 0
+    valid_hours_elapsed = 0
+    percentTime = 0
+    weight = 0
+
+    timeWorked=HoursWorked(timeIn=lastLoggedIn,day = timeNow.strftime("%A"),timeOut=timeNow, totalTime=hours_elapsed, validTime=valid_hours_elapsed, autoLogout=True, percentTime = percentTime, weight = weight, owner = student)
+    timeWorked.save()
+    #add the time worked object to the student so it can be viewed in the calander
+    student.hoursWorked.add(timeWorked)
+    #add the minutes to the student's total time
+    student.save()
+    do_student_calcs(student)
