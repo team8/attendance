810da216dce8e9ea045b36f0427308f934337a1e
Luca Manolache <luca1230@gmail.com>
2021-10-23 12:33:26 -0700

    Now serves static folder

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,6 @@
-use warp::Filter;
-
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let page = warp::get()
-        .and(warp::path::end())
-        .and(warp::fs::file("./website/public/index.html"));
-        // .and(warp::fs::file("./Cargo.toml"));
-
-    warp::serve(page)
+    warp::serve(warp::fs::dir("website/build"))
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
